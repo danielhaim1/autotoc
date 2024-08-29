dafef38b4612b8b6384340fca9b08dc48e8ebdda
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2024-08-29 11:37:43 +0200

    prettify

--- demo/demo.js
+++ demo/demo.js
@@ -1,28 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 	const tocOptions = {
 		contentSelector: ".autotoc-area",
 		navigationContainer: "nav",
 		tocTitle: "Table of Contents",
-		tocIcon: '<i class="icon-toc"><svg><use xlink:href="#icon-toc"></use></svg></i>',
+		tocIcon:
+			'<i class="icon-toc"><svg><use xlink:href="#icon-toc"></use></svg></i>',
 		highlightOffset: 50,
 		headingDepthLimit: 4
 	};
 
-	const externalLinksTop = [{
-		id: "external-link1",
-		text: "Custom Top Link 1"
-	}, {
-		id: "external-link2",
-		text: "Custom Top Link 2"
-	}];
+	const externalLinksTop = [
+		{
+			id: "external-link1",
+			text: "Custom Top Link 1"
+		},
+		{
+			id: "external-link2",
+			text: "Custom Top Link 2"
+		}
+	];
 
-	const externalLinksBottom = [{
-		id: "external-link3",
-		text: "Custom Bottom Link 3"
-	}, {
-		id: "external-link4",
-		text: "Custom Bottom Link 4"
-	}];
+	const externalLinksBottom = [
+		{
+			id: "external-link3",
+			text: "Custom Bottom Link 3"
+		},
+		{
+			id: "external-link4",
+			text: "Custom Bottom Link 4"
+		}
+	];
 
 	try {
 		const tocGenerator = new AutoToc.Generate(...Object.values(tocOptions));
