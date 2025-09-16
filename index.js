dad7fe00fbc35052d21c5a0d14bc8eef1fa00f88
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2025-09-16 13:14:10 +0200

    1.6.1

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { Generate } from './src/index.js';
 
 const AutoToc = { Generate };
 
+// Export for ESM environment
+export default AutoToc;
+
 // Export for Node.js environment
 if (typeof module === 'object' && module.exports) {
     module.exports = AutoToc;
